2cd584ae83e3d7aa930f966ac0ab62e8cd685f4c
Natalie <johnstonenatalie@gmail.com>
2021-04-23 18:10:17 +1200

    initial commut

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        the beauty of dust
+      </header>
+    </div>
+  );
+}
+
+export default App;
